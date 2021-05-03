1b4ddfb73316a037cf19d7489cfc0f035735cb80
Krupal Upadhyay <uukrupal@gmail.com>
2021-05-03 17:35:25 +0530

    q

--- js/main.js
+++ js/main.js
@@ -1,14 +1,52 @@
-/* 
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
 
-Vanilla Template
+   $('.smoothscroll').on('click',function (e) {
+        e.preventDefault();
 
-https://templatemo.com/tm-526-vanilla
+        var target = this.hash,
+        $target = $(target);
 
-*/
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+
+/*----------------------------------------------------*/
+/*  Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+$(window).on('scroll', function() {
+
+        var h = $('header').height();
+        var y = $(window).scrollTop();
+      var nav = $('#nav-wrap');
+
+       if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 1080 ) ) {
+          nav.fadeOut('fast');
+       }
+      else {
+         if (y < h*.20) {
+            nav.removeClass('opaque').fadeIn('fast');
+         }
+         else {
+            nav.addClass('opaque').fadeIn('fast');
+         }
+      }
+
+    });
+
+
+
+
 
 jQuery(document).ready(function($) {
 
-	'use strict';
+    'use strict';
 
     var top_header = $('.parallax-content');
     top_header.css({'background-position':'center center'}); // better use CSS
